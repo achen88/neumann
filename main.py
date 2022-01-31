735a4956bb94d89f65a9ac70f3c8644ebcadc8b8
Andrew Chen <achen88@mit.edu>
2022-01-31 02:06:30 -0500

    working example end to end

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python3
 
 import speech_recognition as sr
+import requests
 
 # obtain audio from the microphone
 r = sr.Recognizer()
 with sr.Microphone() as source:
     print("Say something!")
     audio = r.listen(source, snowboy_configuration=("./snowboy/examples/Python3", ["./snowboy/resources/models/neumann.pmdl"]))
+    # audio = r.listen(source)
 
 # recognize speech using Google Speech Recognition
 try:
     # for testing purposes, we're just using the default API key
     # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
     # instead of `r.recognize_google(audio)`
-    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+    words = r.recognize_google(audio)
+    print("Google Speech Recognition thinks you said " + words)
+    wordsArray = words.split(" ")
+    if wordsArray[1] == "turn":
+        requests.post("https://localhost:3000/trigger", verify=False, json={
+            "trigger": "lights",
+            "data": {
+                "action": wordsArray[2],
+                "target": wordsArray[4],
+            },
+        })
 except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
